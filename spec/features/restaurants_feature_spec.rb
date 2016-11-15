713fc9531a84cbd18ac71515c2dc8efcfcb59315
Nicole <nicoleshasha@hotmail.co.uk>
2016-11-15 11:42:21 +0000

    Add show restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,7 +8,7 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
-    context 'restaurants have been added' do
+  context 'restaurants have been added' do
     before do
       Restaurant.create(name: 'KFC')
     end
@@ -20,13 +20,26 @@ feature 'restaurants' do
     end
   end
   context 'creating restaurants' do
-  scenario 'prompts user to fill out a form, then displays the new restaurant' do
-    visit '/restaurants/new'
-    click_link 'Add a restaurant'
-    fill_in 'Name', with: 'KFC'
-    click_button 'Create Restaurant'
-    expect(page).to have_content 'KFC'
-    expect(current_path).to eq '/restaurants'
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants/new'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
   end
-end
+
+  context 'viewing restaurants' do
+
+    let!(:kfc){ Restaurant.create(name:'KFC') }
+
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'KFC'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+
+  end
 end
