a96cf0f4e429035742f5fcd8316804c02fe5be87
Nicole <nicoleshasha@hotmail.co.uk>
2016-11-16 12:21:17 +0000

    Add validations

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -30,6 +30,17 @@ feature 'restaurants' do
     end
   end
 
+  context 'an invalid restaurant' do
+    scenario 'does not let you submit a name which is too short' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'kf'
+      click_button 'Create Restaurant'
+      expect(page).not_to have_css 'h2', text: 'kf'
+      expect(page).to have_content 'error'
+    end
+  end
+
   context 'viewing restaurants' do
 
     let!(:kfc){ Restaurant.create(name:'KFC') }
@@ -72,5 +83,6 @@ context 'deleting restaurants' do
   end
 
 end
+
 
 end
