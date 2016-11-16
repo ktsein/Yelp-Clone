2003108178c371e27e3ba95fc4015f93569a7d31
Nicole <nicoleshasha@hotmail.co.uk>
2016-11-16 16:51:22 +0000

    Add facebook authentication (not finished)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root "restaurants#index"
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants
 
   resources :restaurants do
     resources :reviews
+
+    devise_scope :user do
+      delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    end
 
   end
 
