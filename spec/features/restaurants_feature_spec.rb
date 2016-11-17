fd20b5d0aa223875205a4a08b5a550b08d515f52
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-11-17 16:53:49 +0000

    Pass all old tests after setting limit on users

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -21,7 +21,12 @@ feature 'restaurants' do
   end
   context 'creating restaurants' do
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
-      visit '/restaurants'
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
       click_button 'Create Restaurant'
@@ -32,7 +37,12 @@ feature 'restaurants' do
 
   context 'an invalid restaurant' do
     scenario 'does not let you submit a name which is too short' do
-      visit '/restaurants'
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'kf'
       click_button 'Create Restaurant'
@@ -59,7 +69,12 @@ feature 'restaurants' do
   before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
 
   scenario 'let a user edit a restaurant' do
-   visit '/restaurants'
+   visit '/'
+   click_link('Sign up')
+   fill_in('Email', with: 'test@example.com')
+   fill_in('Password', with: 'testtest')
+   fill_in('Password confirmation', with: 'testtest')
+   click_button('Sign up')
    click_link 'Edit KFC'
    fill_in 'Name', with: 'Kentucky Fried Chicken'
    fill_in 'Description', with: 'Deep fried goodness'
@@ -76,7 +91,12 @@ context 'deleting restaurants' do
   before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
 
   scenario 'removes a restaurant when a user clicks a delete link' do
-    visit '/restaurants'
+    visit '/'
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
     click_link 'Delete KFC'
     expect(page).not_to have_content 'KFC'
     expect(page).to have_content 'Restaurant deleted successfully'
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -4,12 +4,17 @@ feature 'reviewing' do
   before { Restaurant.create name: 'KFC'}
 
   scenario 'allows users to leave a review using a form' do
-     visit '/restaurants'
+     visit '/'
+     click_link('Sign up')
+     fill_in('Email', with: 'test@example.com')
+     fill_in('Password', with: 'testtest')
+     fill_in('Password confirmation', with: 'testtest')
+     click_button('Sign up')
      click_link 'Rate KFC'
      fill_in "Thoughts", with: "so so"
      select '3', from: 'Rating'
      click_button 'Leave Review'
-     expect(current_path).to eq '/restaurants'
+     click_link 'KFC'
      expect(page).to have_content('so so')
   end
 
